670606e9f5b1a2755ef69498470e0ce94a3036e0
franciscolir <francisco.lira.b@gmail.com>
2024-04-30 19:59:30 -0400

    "avance en menu usuario"

--- src/com/alura/conversormonedas/principal/MenuInfo.java
+++ src/com/alura/conversormonedas/principal/MenuInfo.java
@@ -1,4 +1,62 @@
 package com.alura.conversormonedas.principal;
 
+import com.alura.conversormonedas.api.ConsultaMoneda;
+import com.alura.conversormonedas.monedas.Moneda;
+import com.alura.conversormonedas.monedas.ParametrosDeConversion;
+
+import java.util.Scanner;
+
 public class MenuInfo {
+
+    String monedaBase;
+    String monedaACambiar;
+
+    Scanner ingresaCodigo = new Scanner(System.in);
+    ConsultaMoneda consultaMoneda = new ConsultaMoneda();
+    ParametrosDeConversion parametros = new ParametrosDeConversion();
+    public void bienvenida(){
+        System.out.println("*************************************");
+        System.out.println("Bienvenido al conversor de monedas");
+        System.out.println("*************************************");
+    }
+
+
+    public void listaMonedas (){
+        System.out.println("""
+                Monedas disponibles:
+                
+                Dolar USA       --> USD
+                Real Brasileño  --> BRL
+                Peso Argentino  --> ARS
+                Peso Chileno    --> CLP
+                Euro            --> EUR
+                """);
+    }
+    public void menuPrincipal(){
+        System.out.println("""
+                
+                Menu inicial:
+                
+                1-Convertir moneda
+                2-Hisotial de conversiones
+                9-Salir
+                """);
+        }
+    public void seleccionMonedas() {
+        System.out.println("Ingrese codigo de moneda inicial:");
+        monedaBase = ingresaCodigo.nextLine().toUpperCase();
+        System.out.println("Ingrese moneda a cambiar");
+        monedaACambiar = ingresaCodigo.nextLine().toUpperCase();
+        System.out.println("Ingrese cantidad a cambiar");
+        var cantidad = Integer.valueOf(ingresaCodigo.nextInt());
+        parametros.setCantidadACambiar(cantidad);
+
+    }
+    public void conversionDeMonedas(){
+
+        Moneda moneda = consultaMoneda.buscaMoneda(monedaBase, monedaACambiar);
+        parametros.calculadora(moneda.conversion_rate());
+        System.out.println(parametros.getCantidadACambiar() + " " + monedaBase + " equivalen a : " + parametros.getTotalConversion() + " " + monedaACambiar);
+        System.out.println("fecha conversion: " + moneda.time_last_update_utc());
+    }
 }
--- src/com/alura/conversormonedas/principal/Principal.java
+++ src/com/alura/conversormonedas/principal/Principal.java
@@ -8,52 +8,32 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        int opcion = 0;
+
         Scanner ingresaCodigo = new Scanner(System.in);
         ConsultaMoneda consultaMoneda = new ConsultaMoneda();
         ParametrosDeConversion parametros = new ParametrosDeConversion();
-        System.out.println("*************************************");
-        System.out.println("Bienvenido al conversor de monedas");
-        System.out.println("********");
-        System.out.println("""
-                Menu inicial:
-                
-                1-Convertir moneda
-                2-Hisotial de conversiones
-                9-Salir
-                """);
-        while (opcion != 9)
-
+     MenuInfo menuInfo = new MenuInfo();
+     menuInfo.bienvenida();
+        int opcion = 0;
+        while (opcion != 9) {
+            menuInfo.menuPrincipal();
             opcion = ingresaCodigo.nextInt();
-
-        var monedaBase = String.valueOf(ingresaCodigo.nextLine());
             switch (opcion) {
                 case 1:
-                    System.out.println("Ingrese codigo de moneda inicial:");
-                    System.out.println("""
-                Dolar USA       --> USD
-                Real Brasileño  --> BRL
-                Peso Argentino  --> ARS
-                Peso Chileno    --> CLP
-                Euro            --> EUR
-                """);
-                    System.out.println("Ingrese moneda a cambiar");
-                    var monedaACambiar = String.valueOf(ingresaCodigo.nextLine());
-                    System.out.println("Ingrese cantidad a cambiar");
-                    var cantidad = Integer.valueOf(ingresaCodigo.nextInt());
-                    parametros.setCantidadACambiar(cantidad);
-                    Moneda moneda = consultaMoneda.buscaMoneda(monedaBase, monedaACambiar);
-                    parametros.calculadora(moneda.conversion_rate());
-                    System.out.println(parametros.getCantidadACambiar() + monedaBase + " equivalen a : " + parametros.getTotalConversion() + monedaACambiar);
-                    System.out.println("fecha conversion: " + moneda.time_last_update_utc());
+                    menuInfo.listaMonedas();
+                    menuInfo.seleccionMonedas();
+                    menuInfo.conversionDeMonedas();
+                    break;
                 case 2:
+                    System.out.println("opcion2");
+                    break;
                 case 9:
                     System.out.println("Gracias por utilizar nuestros servicios");
                     break;
 
                 default:
                     System.out.println("Opcion no valida");
-
+            }
             }
             }
 
